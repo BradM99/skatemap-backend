f08364c0490e32c6a645428a1b1359955167f2e5
bradley <55547314+BradM99@users.noreply.github.com>
2026-06-16 21:38:20 +0100

    updated user_db operations to search by ID instead of username all the time

--- database/users_db.py
+++ database/users_db.py
@@ -1,17 +1,17 @@
-# Database operations for users
+from uuid import UUID
 from sqlalchemy.orm import Session
 from database.models import User
 
 
-def create_user(db: Session, username: str, email: str, password: str) -> None:
-    user = User(username=username, email=email, password=password)
+def create_user(db: Session, username: str, email: str, hashed_password: str) -> User:
+    user = User(username=username, email=email, hashed_password=hashed_password)
     db.add(user)
     db.commit()
     db.refresh(user)
     return user
 
 
-def get_user(db: Session, username: str) -> User | None:
+def get_user_by_username(db: Session, username: str) -> User | None:
     return db.query(User).filter(User.username == username).first()
 
 
@@ -19,12 +19,17 @@ def get_user_by_email(db: Session, email: str) -> User | None:
     return db.query(User).filter(User.email == email).first()
 
 
-def get_all_users(db: Session) -> list[type[User]]:
+def get_user_by_id(db: Session, user_id: UUID) -> User | None:
+    return db.query(User).filter(User.id == user_id).first()
+
+
+def get_all_users(db: Session) -> list[User]:
     return db.query(User).all()
 
 
-def delete_user(db: Session, username: str) -> None:
-    user = get_user(db, username)
+def delete_user(db: Session, user_id: UUID) -> None:
+    user = get_user_by_id(db, user_id)
+
     if user:
         db.delete(user)
         db.commit()
